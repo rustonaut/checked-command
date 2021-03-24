d3b6c23b6f5d7a4a2cda7085b52309fca8deb4ed
Philipp Korber <philipp@korber.dev>
2021-03-24 01:33:51 +0100

    Updated examples/mock.

--- examples/mock.rs
+++ examples/mock.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 
-use mapped_command::{Command, MapStdoutAndErrStrings, MapStdoutString};
+use mapped_command::{
+    Command, ExecResult, MapStdoutAndErrStrings, MapStdoutString, ProcessPipeSetting,
+};
 use proptest::{prelude::*, test_runner::FileFailurePersistence};
 
 /// Creates a command calling "foobar".
@@ -8,8 +10,6 @@ use proptest::{prelude::*, test_runner::FileFailurePersistence};
 /// If magic is true the stdout is parsed as u32,
 /// if it is false stdout and stderr are both parsed
 /// as u32 and added together (yes I know that is stupid ;=) )
-//TODO
-#[allow(unused)]
 fn create_foobar_command(magic: bool) -> Command<u32, Box<dyn Error>> {
     // Command is only generic over the output/error type
     // so you can multiplex over different commands with
@@ -39,45 +39,49 @@ proptest! {
     })]
 
     fn test_mocked_foobar(
-        _magic in any::<bool>(),
-        _stdout_num in any::<u32>(),
-        _stderr_num in any::<u32>()
+        magic in any::<bool>(),
+        stdout_num in any::<u32>(),
+        stderr_num in any::<u32>()
     ) {
-        // prop_assume!(stdout_num as u64 + stderr_num as u64 <= u32::MAX as u64);
+        prop_assume!(stdout_num as u64 + stderr_num as u64 <= u32::MAX as u64);
 
-        // // create it somewhere
-        // let num = create_foobar_command(magic)
-        //     //mock the command and only return stdout/err if it would be captured
-        //     .with_exec_replacement_callback(move |options| {
-        //         let stdout = if ret_set.capture_stdout() {
-        //             Some(stdout_num.to_string().into())
-        //         } else {
-        //             None
-        //         };
-        //         let stderr = if ret_set.capture_stderr() {
-        //             Some(stderr_num.to_string().into())
-        //         } else {
-        //             None
-        //         };
-        //         Ok(ExecResult {
-        //             exit_status: 0.into(),
-        //             stdout,
-        //             stderr,
-        //         })
-        //     })
-        //     .run()
-        //     .unwrap();
+        // create it somewhere
+        let num = create_foobar_command(magic)
+            //mock the command and only return stdout/err if it would be captured
+            .with_exec_replacement_callback(move |options| {
+                let stdout = options.override_stdout.and_then(|stdout| {
+                    if let ProcessPipeSetting::Piped = stdout {
+                        Some(stdout_num.to_string().into())
+                    } else {
+                        None
+                    }
+                });
+                let stderr = options.override_stderr.and_then(|stderr| {
+                    if let ProcessPipeSetting::Piped = stderr {
+                        Some(stderr_num.to_string().into())
+                    } else {
+                        None
+                    }
+                });
 
-        // if magic {
-        //     assert_eq!(num, stdout_num);
-        // } else {
-        //     assert_eq!(num, stdout_num+stderr_num);
-        // }
+                Ok(ExecResult {
+                    exit_status: 0.into(),
+                    stdout,
+                    stderr,
+                })
+            })
+            .run()
+            .unwrap();
+
+        if magic {
+            assert_eq!(num, stdout_num);
+        } else {
+            assert_eq!(num, stdout_num+stderr_num);
+        }
     }
 }
 
 //overhead from proptest, normally we would use `proptest` but we do want to run main.
 fn main() {
-    //TODO
-    // test_mocked_foobar()
+    test_mocked_foobar()
 }
